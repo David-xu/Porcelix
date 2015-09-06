9f6158cfc81bf3844c4707020184a52f52b0800e
david <stone.dawei@gmail.com>
2015-09-06 16:44:16 +0800

    Update ml_string.h

--- myLoader/include/ml_string.h
+++ myLoader/include/ml_string.h
@@ -1,10 +1,10 @@
 #ifndef _ML_STRING_H_
 #define _ML_STRING_H_
 
-void memcpy(void *dst, void *src, u16 len);
-void memset(void *dst, u8 value, u16 len);
+void memcpy(void *dst, void *src, u32 len);
+void memset(void *dst, u8 value, u32 len);
 void memset_u16(u16 *dst, u16 value, u16 len);
-
+int strcmp(const char *str1, const char *str2);
 int memcmp(void *str1, void *str2, u32 len);
 u16 strlen(char *str);
 
