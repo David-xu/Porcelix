85708f4a134cb0fb0f5d690a295f7b0ef5744e2b
david <stone.dawei@gmail.com>
2015-09-06 16:48:23 +0800

    Update task.h

--- myLoader/include/task.h
+++ myLoader/include/task.h
@@ -4,6 +4,7 @@
 #include "typedef.h"
 #include "list.h"
 #include "memory.h"
+#include "section.h"
 
 #define TASKSTACK_RANGE         BUDDY_RANK_8K
 #define TASKSTACK_SIZE          (PAGE_SIZE << TASKSTACK_RANGE)
@@ -15,22 +16,58 @@ typedef enum _task_state {
     STATE_EXIT
 } task_state_e;
 
+#define		TASKFLAG_KT				(0x00000001)			/* Kernel thread */
+
 struct _task_stack;
 typedef struct _task {
-    task_state_e    state;
-    int     pid;
+	task_state_e    state;
+	int     pid;
+	u32		tskflag;
 
-    u32     ctx_ip, ctx_sp;
+	u32     ctx_ip, ctx_sp;
 
-    struct list_head q;         /* link this task desc into q */
-    
-    struct _task_stack *stack;
+	u32		pgd_pa;				/* task page directory phy addr */
+
+	struct list_head q;			/* link this task desc into q */
+
+	struct _task_stack *stack;
 } task_t;
+
+typedef struct _tss {
+	unsigned short		back_link, __blh;
+	unsigned long		sp0;
+	unsigned short		ss0, __ss0h;
+	unsigned long		sp1;
+	/* ss1 caches MSR_IA32_SYSENTER_CS: */
+	unsigned short		ss1, __ss1h;
+	unsigned long		sp2;
+	unsigned short		ss2, __ss2h;
+	unsigned long		__cr3;
+	unsigned long		ip;
+	unsigned long		flags;
+	unsigned long		ax;
+	unsigned long		cx;
+	unsigned long		dx;
+	unsigned long		bx;
+	unsigned long		sp;
+	unsigned long		bp;
+	unsigned long		si;
+	unsigned long		di;
+	unsigned short		es, __esh;
+	unsigned short		cs, __csh;
+	unsigned short		ss, __ssh;
+	unsigned short		ds, __dsh;
+	unsigned short		fs, __fsh;
+	unsigned short		gs, __gsh;
+	unsigned short		ldt, __ldth;
+	unsigned short		trace;			/* LSB indicate the 'Debug Trace' */
+	unsigned short		io_bitmap_base;
+} tss_t;
 
 asmlinkage task_t *__switch_to(task_t *prev, task_t *next);
 
 typedef struct _task_stack {
-    task_t task_space, *task;           /* now we just put the task at the head of stack */
+    task_t *task;
 } task_stack_t;
 
 static inline task_stack_t *getcurstack(void)
@@ -76,20 +113,27 @@ static inline task_stack_t *getcurstack(void)
 #define current         (getcurstack()->task)
 
 void schedule(void);
+void tail_sched(void);
 void exit(int exit_code);
 
-typedef int (*task_entry)(void *param);
+typedef asmlinkage int (*task_entry)(struct pt_regs *regs, void *param);
 
 /* ring 0 thread */
 int kernel_thread(task_entry entry, void *param);
+int exec_test();
 
 void idleloop(void);
+
+void kt_after_switch(void);
 
 
 /* sleep
  * usec: micro second
  */
 void sleep(int usec);
+void wait_ms(u32 ms);			/* busy loop */
+
+extern volatile u32	smp_nap;
 
 #endif
 
