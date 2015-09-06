96df46dd3c12f378c5af10fdfb00a5e8c02b94a9
david <stone.dawei@gmail.com>
2015-09-06 16:42:25 +0800

    Update interrupt.h

--- myLoader/include/interrupt.h
+++ myLoader/include/interrupt.h
@@ -4,6 +4,82 @@
 #include "section.h"
 #include "list.h"
 #include "typedef.h"
+#include "io.h"
+#include "desc.h"
+
+struct pt_regs {
+	long ebx;
+	long ecx;
+	long edx;
+	long esi;
+	long edi;
+	long ebp;
+	long eax;
+	int  xds;
+	int  xes;
+	int  xfs;
+	int  xgs;
+	long orig_eax;
+	long eip;
+	int  xcs;
+	long eflags;
+	long esp;
+	int  xss;
+};
+
+static inline void dump_eflags(u32 eflags)
+{
+	printf("eflags: 0x%#8x\n%s %s %s %s %s %s %s IOPL:%d %s %s %s %s %s %s %s %s %s\n",
+		   eflags,
+		   (eflags & EFLAGSMASK_ID) ? "ID" : "id",
+		   (eflags & EFLAGSMASK_VIP) ? "VIP" : "vip",
+		   (eflags & EFLAGSMASK_VIF) ? "VIF" : "vif",
+		   (eflags & EFLAGSMASK_AC) ? "AC" : "ac",
+		   (eflags & EFLAGSMASK_VM) ? "VM" : "vm",
+		   (eflags & EFLAGSMASK_RF) ? "RF" : "rf",
+		   (eflags & EFLAGSMASK_NT) ? "NT" : "nt",
+		   (eflags & EFLAGSMASK_IOPL) >> 12,
+		   (eflags & EFLAGSMASK_OF) ? "OF" : "of",
+		   (eflags & EFLAGSMASK_DF) ? "DF" : "df",
+		   (eflags & EFLAGSMASK_IF) ? "IF" : "if",
+		   (eflags & EFLAGSMASK_TF) ? "TF" : "tf",
+		   (eflags & EFLAGSMASK_SF) ? "SF" : "sf",
+		   (eflags & EFLAGSMASK_ZF) ? "ZF" : "zf",
+		   (eflags & EFLAGSMASK_AF) ? "AF" : "af",
+		   (eflags & EFLAGSMASK_PF) ? "PF" : "pf",
+		   (eflags & EFLAGSMASK_CF) ? "CF" : "cf");
+}
+
+static inline void dump_ptregs(struct pt_regs *regs)
+{
+	u32 cur_cs, cur_ss, cur_esp;
+	__asm__ __volatile__ (
+		"movl	%%cs, %%eax			\n\t"
+		"movl	%%eax, %0			\n\t"
+		"movl	%%ss, %%eax			\n\t"
+		"movl	%%eax, %1			\n\t"
+		"movl	%%esp, %2			\n\t"
+		:"=m"(cur_cs), "=m"(cur_ss), "=m"(cur_esp)
+		:
+		:"%eax"
+	);
+	printf("\n\n"
+		   "ebx:0x%#8x, ecx:0x%#8x, edx:0x%#8x\n"
+		   "esi:0x%#8x, edi:0x%#8x, ebp:0x%#8x\n"
+		   " ds:0x%#4x,      es:0x%#4x,      fs:0x%#4x,  gs:0x%#8x(entry function)\n"
+		   "eip:0x%#8x,  cs:0x%#4x,  osp:0x%#8x, oss:0x%#4x, orig_eax:0x%#8x\n",
+		   regs->ebx, regs->ecx, regs->edx,
+		   regs->esi, regs->edi, regs->ebp,
+		   (u16)(regs->xds), (u16)(regs->xes), (u16)(regs->xfs), regs->xgs,		/* this is the entry function addr */
+		   regs->eip, (u16)(regs->xcs), regs->esp, regs->xss, regs->orig_eax);
+
+	dump_eflags(regs->eflags);
+
+	if ((cur_cs & 0x3) == ((regs->xcs) & 0x3))
+	{
+		printf("current ss:0x%#4x, esp:0x%#8x\n", (u16)cur_ss, cur_esp);
+	}
+}
 
 #define _cli()      do {asm volatile("cli":::"memory");} while (0)
 #define _sti()      do {asm volatile("sti":::"memory");} while (0)
@@ -50,8 +126,10 @@ static inline void native_restore_fl(u32 flags)
 #define PORT_8259A_SLAVER_A0            (0xA0)
 #define PORT_8259A_SLAVER_A1            (0xA1)
 
+#define	INTPROC_DEFINE(vect)				\
+	extern void intproc_##vect(void);
 
-typedef void (*intproc_ent)(void *param);
+typedef void (*intproc_ent)(struct pt_regs *regs, void *param);
 
 typedef struct interrupt_proc {
     struct list_head int_list;          /* interrupt list */
@@ -59,16 +137,18 @@ typedef struct interrupt_proc {
     void *param;
 } interrupt_proc_t;
 
-extern void int10_public_entrance(void);
-extern void kbd_int_entrance(void);
-extern void hd_int_entrance(void);
-
 extern void lapictimer_entrance(void);
 
 
-void interrupt_init() _SECTION_(.init.text);
+void interrupt_init();
 
 int interrup_register(int irq, intproc_ent intf, void *param);
+
+void trap_init(void);
+
+asmlinkage void intprocjmp_master(struct pt_regs *regs, u32 irq);
+asmlinkage void intprocjmp_slaver(struct pt_regs *regs, u32 irq);
+
 
 #endif
 
