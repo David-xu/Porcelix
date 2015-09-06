d0ddf25e5b46f7256386b7598874752b9e5d948b
david <stone.dawei@gmail.com>
2015-09-06 16:09:30 +0800

    Update corestart16.c

--- myLoader/core/corestart16.c
+++ myLoader/core/corestart16.c
@@ -4,14 +4,12 @@
 #include "hd.h"
 #include "memory.h"
 
-__asm__(".code16gcc\n");
+asm(".code16gcc\n");
+
 
 extern struct bootparam bootparam;
 extern struct hd_info hd0_info;
 extern struct hd_info hd1_info;
-
-extern void jump2pe(void);
-
 
 static u16 read16(u16 ds, u16 off)
 {
@@ -51,12 +49,87 @@ static void move_16(u32 dst, u32 src, u16 size)
 	);
 }
 
+/* just output any byte to port 0x80, we used it to delay */
+static inline void io_delay_16(void)
+{
+	const u16 DELAY_PORT = 0x80;
+	asm volatile("outb %%al,%0" : : "dN" (DELAY_PORT));
+}
+
+/* Basic port I/O */
+static inline void outb_16(u8 v, u16 port)
+{
+	asm volatile("outb %0,%1" : : "a" (v), "dN" (port));
+}
+static inline u8 inb_16(u16 port)
+{
+	u8 v;
+	asm volatile("inb %1,%0" : "=a" (v) : "dN" (port));
+	return v;
+}
+
+/* read until the 8042's buff get empty
+   I just copy the code from the linux kernel :) */
+#define MAX_8042_LOOPS__	100000
+#define MAX_8042_FF__	32
+static int _SECTION_(.init.text) empty_8042_16(void)
+{
+	u8 status;
+	int loops = MAX_8042_LOOPS__;
+	int ffs   = MAX_8042_FF__;
+
+	while (loops--) {
+		io_delay_16();
+
+		status = inb_16(0x64);
+		if (status == 0xff) {
+			/* FF is a plausible, but very unlikely status */
+			if (!--ffs)
+				return -1; /* Assume no KBC present */
+		}
+		if (status & 1) {
+			/* Read and discard input data */
+			io_delay_16();
+			(void)inb_16(0x60);
+		} else if (!(status & 2)) {
+			/* Buffers empty, finished! */
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+
+static u8 _SECTION_(.init.text) kbdctrl_outport_r_16(void)
+{
+    outb_16(0xD0, 0x64);
+    io_delay_16();
+    return inb_16(0x60);
+}
+
+static void _SECTION_(.init.text) kbdctrl_outport_w_16(u8 cmd)
+{
+	empty_8042_16();
+
+    outb_16(0xD1, 0x64);
+
+	empty_8042_16();
+
+    outb_16(cmd, 0x60);
+}
+
+void _SECTION_(.init.text) disableA20_16()
+{
+	kbdctrl_outport_w_16(kbdctrl_outport_r_16() & 0xFD);
+}
 
 /* entry of the core.elf */
 void _SECTION_(.init.text) corestart_16c(void)
 {
 	u32 coresize, cs;
-	u32 dstaddr = 0, srcaddr = IMGCORE_LOADADDR;
+	u32 dstaddr = 0x1000, srcaddr = IMGCORE_LOADADDR + 0x1000;
+
 	/* step 1: get some info from boot.bin, the first 512B */
 	move_16(IMGCORE_LOADADDR + (u32)(&bootparam),
 			(BOOTPARAM_PACKADDR_BASS << 4) | BOOTPARAM_PACKADDR_OFFSET,
@@ -98,6 +171,13 @@ void _SECTION_(.init.text) corestart_16c(void)
 		:"a"((IMGCORE_LOADADDR + (u32)(&raminfo_buf)) >> 4),
 		 "D"((IMGCORE_LOADADDR + (u32)(&raminfo_buf)) & 0xF)
 		:"memory"
+	);
+
+	/* step4. backup current IDRT, it's the BIOS interrupt vect tbale */
+	__asm__ __volatile__ (
+		"sidt	%0				\n\t"
+		:"=m"(bootparam.idtr_back)
+		:
 	);
 
 	/* move the core from
