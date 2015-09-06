901ca53debc8156f9f6ef60c17739848ba0280f9
david <stone.dawei@gmail.com>
2015-09-06 16:57:26 +0800

    Update uploadml.sh

--- myLoader/udp_cli/uploadml.sh
+++ myLoader/udp_cli/uploadml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dd if=../../img/sys.img of=tmp.img bs=512 count=128
-./pc_client 192.168.1.20 sendfile hd1 tmp.img 0
+dd if=../../disk.img of=tmp.img bs=512 count=128
+./pc_client 220.1.1.20 sendfile hd0 tmp.img 0
